114e8a5c74fccf56303afde25c5a7fce7b2c79e3
hutingting119 <1912257350@qq.com>
2018-09-20 17:49:22 +0800

    add

--- web/viewer.js
+++ web/viewer.js
@@ -3247,9 +3247,6 @@
             if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
         }
 
-        ;
-        var GenericCom = {};
-
         var GenericPreferences = function (_BasePreferences) {
             _inherits(GenericPreferences, _BasePreferences);
 
@@ -3291,220 +3288,16 @@
             return new _genericl10n.GenericL10n(_pdfjsLib.PDFJS.locale);
         };
         _app.PDFViewerApplication.externalServices = GenericExternalServices;
-        // exports.GenericCom = GenericCom;
-
-        /***/
     }),
      (function (module, exports, __webpack_require__) {
 
-        "use strict";
-
-
-        Object.defineProperty(exports, "__esModule", {
-            value: true
-        });
-        exports.PDFPrintService = undefined;
-
-        var _ui_utils = __webpack_require__(0);
 
         var _app = __webpack_require__(4);
 
-        var _pdfjsLib = __webpack_require__(1);
-
-        var activeService = null;
-        var overlayManager = null;
-
-        function renderPage(activeServiceOnEntry, pdfDocument, pageNumber, size) {
-            var scratchCanvas = activeService.scratchCanvas;
-            var PRINT_RESOLUTION = 150;
-            var PRINT_UNITS = PRINT_RESOLUTION / 72.0;
-            scratchCanvas.width = Math.floor(size.width * PRINT_UNITS);
-            scratchCanvas.height = Math.floor(size.height * PRINT_UNITS);
-            var width = Math.floor(size.width * _ui_utils.CSS_UNITS) + 'px';
-            var height = Math.floor(size.height * _ui_utils.CSS_UNITS) + 'px';
-            var ctx = scratchCanvas.getContext('2d');
-            ctx.save();
-            ctx.fillStyle = 'rgb(255, 255, 255)';
-            ctx.fillRect(0, 0, scratchCanvas.width, scratchCanvas.height);
-            ctx.restore();
-            return pdfDocument.getPage(pageNumber).then(function (pdfPage) {
-                var renderContext = {
-                    canvasContext: ctx,
-                    transform: [PRINT_UNITS, 0, 0, PRINT_UNITS, 0, 0],
-                    viewport: pdfPage.getViewport(1, size.rotation),
-                    intent: 'print'
-                };
-                return pdfPage.render(renderContext).promise;
-            }).then(function () {
-                return {
-                    width: width,
-                    height: height
-                };
-            });
-        }
-
-        function PDFPrintService(pdfDocument, pagesOverview, printContainer, l10n) {
-            this.pdfDocument = pdfDocument;
-            this.pagesOverview = pagesOverview;
-            this.l10n = l10n || _ui_utils.NullL10n;
-            this.currentPage = -1;
-            this.scratchCanvas = document.createElement('canvas');
-        }
-
-        PDFPrintService.prototype = {
-            layout: function layout() {
-                this.throwIfInactive();
-                var body = document.querySelector('body');
-                body.setAttribute('data-pdfjsprinting', true);
-                var hasEqualPageSizes = this.pagesOverview.every(function (size) {
-                    return size.width === this.pagesOverview[0].width && size.height === this.pagesOverview[0].height;
-                }, this);
-                if (!hasEqualPageSizes) {
-                    console.warn('Not all pages have the same size. The printed ' + 'result may be incorrect!');
-                }
-                this.pageStyleSheet = document.createElement('style');
-                var pageSize = this.pagesOverview[0];
-                this.pageStyleSheet.textContent = '@supports ((size:A4) and (size:1pt 1pt)) {' + '@page { size: ' + pageSize.width + 'pt ' + pageSize.height + 'pt;}' + '}';
-                body.appendChild(this.pageStyleSheet);
-            },
-            destroy: function destroy() {
-                if (activeService !== this) {
-                    return;
-                }
-                if (this.pageStyleSheet && this.pageStyleSheet.parentNode) {
-                    this.pageStyleSheet.parentNode.removeChild(this.pageStyleSheet);
-                    this.pageStyleSheet = null;
-                }
-                this.scratchCanvas.width = this.scratchCanvas.height = 0;
-                this.scratchCanvas = null;
-                activeService = null;
-                ensureOverlay().then(function () {
-                    if (overlayManager.active !== 'printServiceOverlay') {
-                        return;
-                    }
-                    overlayManager.close('printServiceOverlay');
-                });
-            },
-            renderPages: function renderPages() {
-                var _this = this;
-
-                var pageCount = this.pagesOverview.length;
-                var renderNextPage = function renderNextPage(resolve, reject) {
-                    _this.throwIfInactive();
-                    if (++_this.currentPage >= pageCount) {
-                        renderProgress(pageCount, pageCount, _this.l10n);
-                        resolve();
-                        return;
-                    }
-                    var index = _this.currentPage;
-                    renderProgress(index, pageCount, _this.l10n);
-                    renderPage(_this, _this.pdfDocument, index + 1, _this.pagesOverview[index]).then(_this.useRenderedPage.bind(_this)).then(function () {
-                        renderNextPage(resolve, reject);
-                    }, reject);
-                };
-                return new Promise(renderNextPage);
-            },
-            useRenderedPage: function useRenderedPage(printItem) {
-                this.throwIfInactive();
-                var img = document.createElement('img');
-                img.style.width = printItem.width;
-                img.style.height = printItem.height;
-                var scratchCanvas = this.scratchCanvas;
-                if ('toBlob' in scratchCanvas && !_pdfjsLib.PDFJS.disableCreateObjectURL) {
-                    scratchCanvas.toBlob(function (blob) {
-                        img.src = URL.createObjectURL(blob);
-                    });
-                } else {
-                    img.src = scratchCanvas.toDataURL();
-                }
-                var wrapper = document.createElement('div');
-                wrapper.appendChild(img);
-                return new Promise(function (resolve, reject) {
-                    img.onload = resolve;
-                    img.onerror = reject;
-                });
-            },
-            performPrint: function performPrint() {
-                var _this2 = this;
-
-                this.throwIfInactive();
-                return new Promise(function (resolve) {
-                    setTimeout(function () {
-                        if (!_this2.active) {
-                            resolve();
-                            return;
-                        }
-                        print.call(window);
-                        setTimeout(resolve, 20);
-                    }, 0);
-                });
-            },
-
-            get active() {
-                return this === activeService;
-            },
-            throwIfInactive: function throwIfInactive() {
-                if (!this.active) {
-                    throw new Error('This print request was cancelled or completed.');
-                }
-            }
-        };
-        var print = window.print;
-        window.print = function print() {
-            if (activeService) {
-                console.warn('Ignored window.print() because of a pending print job.');
-                return;
-            }
-            ensureOverlay().then(function () {
-                if (activeService) {
-                    overlayManager.open('printServiceOverlay');
-                }
-            });
-            try {
-                dispatchEvent('beforeprint');
-            } finally {
-                if (!activeService) {
-                    console.error('Expected print service to be initialized.');
-                    ensureOverlay().then(function () {
-                        if (overlayManager.active === 'printServiceOverlay') {
-                            overlayManager.close('printServiceOverlay');
-                        }
-                    });
-                    return;
-                }
-                var activeServiceOnEntry = activeService;
-                activeService.renderPages().then(function () {
-                    return activeServiceOnEntry.performPrint();
-                }).catch(function () {
-                }).then(function () {
-                    if (activeServiceOnEntry.active) {
-                        abort();
-                    }
-                });
-            }
-        };
         function dispatchEvent(eventType) {
             var event = document.createEvent('CustomEvent');
             event.initCustomEvent(eventType, false, false, 'custom');
             window.dispatchEvent(event);
-        }
-
-        function abort() {
-            if (activeService) {
-                activeService.destroy();
-                dispatchEvent('afterprint');
-            }
-        }
-
-        function renderProgress(index, total, l10n) {
-            var progressContainer = document.getElementById('printServiceOverlay');
-            var progress = Math.round(100 * index / total);
-            var progressBar = progressContainer.querySelector('progress');
-            var progressPerc = progressContainer.querySelector('.relative-progress');
-            progressBar.value = progress;
-            l10n.get('print_progress_percent', {progress: progress}, progress + '%').then(function (msg) {
-                progressPerc.textContent = msg;
-            });
         }
 
         var hasAttachEvent = !!document.attachEvent;
@@ -3523,49 +3316,12 @@
                 return;
             }
         }, true);
-        if (hasAttachEvent) {
-            document.attachEvent('onkeydown', function (event) {
-                event = event || window.event;
-                if (event.keyCode === 80 && event.ctrlKey) {
-                    event.keyCode = 0;
-                    return false;
-                }
-            });
-        }
-        if ('onbeforeprint' in window) {
-            var stopPropagationIfNeeded = function stopPropagationIfNeeded(event) {
-                if (event.detail !== 'custom' && event.stopImmediatePropagation) {
-                    event.stopImmediatePropagation();
-                }
-            };
-            window.addEventListener('beforeprint', stopPropagationIfNeeded);
-            window.addEventListener('afterprint', stopPropagationIfNeeded);
-        }
-        var overlayPromise;
-
-        function ensureOverlay() {
-            if (!overlayPromise) {
-                overlayManager = _app.PDFViewerApplication.overlayManager;
-                if (!overlayManager) {
-                    throw new Error('The overlay manager has not yet been initialized.');
-                }
-                overlayPromise = overlayManager.register('printServiceOverlay', document.getElementById('printServiceOverlay'), abort, true);
-                document.getElementById('printCancel').onclick = abort;
-            }
-            return overlayPromise;
-        }
-
         _app.PDFPrintServiceFactory.instance = {
             supportsPrinting: true,
         };
-        exports.PDFPrintService = PDFPrintService;
-
-        /***/
     }),
+
      (function (module, exports, __webpack_require__) {
-
-        "use strict";
-
 
         document.webL10n = function (window, document, undefined) {
             var gL10nData = {};
@@ -4285,12 +4041,6 @@
                 getData: function getData() {
                     return gL10nData;
                 },
-                getText: function getText() {
-                    return gTextData;
-                },
-                getLanguage: function getLanguage() {
-                    return gLanguage;
-                },
                 setLanguage: function setLanguage(lang, callback) {
                     loadLocale(lang, function () {
                         if (callback) callback();
@@ -4302,9 +4052,7 @@
                     return rtlList.indexOf(shortCode) >= 0 ? 'rtl' : 'ltr';
                 },
                 translate: translateFragment,
-                getReadyState: function getReadyState() {
-                    return gReadyState;
-                },
+
                 ready: function ready(callback) {
                     if (!callback) {
                         return;
@@ -4322,7 +4070,6 @@
             };
         }(window, document);
 
-        /***/
     }),
     /* 11 */
     /***/ (function (module, exports, __webpack_require__) {
